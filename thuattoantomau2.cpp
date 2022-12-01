74bb31b81e2591fed31d0a4cc6a57854d7ea4acc
nhan2003ct <117053506+nhan2003ct@users.noreply.github.com>
2022-12-01 21:21:56 +0700

    Update thuattoantomau2.cpp

--- thuattoantomau2.cpp
+++ thuattoantomau2.cpp
@@ -1,91 +1,193 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<graphics.h>
-#include<iostream>
+#include <conio.h>
+#include <iostream>
+#include <graphics.h>
+
 using namespace std;
 
-struct ToaDo
+struct ToaDo2D
+
 {
- int x,y;
+
+  int x,y;
+
 };
- void vedagiac(ToaDo p[],int v)
+
+void Nhap(int &n,ToaDo2D a[])
+
+{
+
+  cout<<"\nNhap so dinh cho da giac:"; cin>>n;
+
+  for(int i=1;i<=n;i++)
+
+  {
+
+    cout<<"\n a["<<i<<"].x=";  cin>>a[i].x;
+
+    cout<<"\n a["<<i<<"].y=";  cin>>a[i].y;
+
+  }
+
+}
+
+void VeDaGiac(int n,ToaDo2D a[])
+
+{
+
+  int i,j;
+
+  for (i=1;i<=n;i++)
+
+    {
+
+      if (i==n) j=1; else j=i+1;
+
+      line(a[i].x,a[i].y,a[j].x,a[j].y);
+
+    }
+
+}
+
+int min(int a,int b)
+
+{
+
+  return (a<b)?a:b;
+
+}
+
+int max(int a,int b)
+
+{
+
+  return (a>b)?a:b;
+
+}
+
+void Scanline(int n,ToaDo2D a[])
+
+{
+
+ //Tim minx, maxx
+
+ int x,minx,maxx;
+
+ minx=a[1].x;
+
+ maxx=a[1].x;
+
+ for (int i=2;i<=n;i++)
+
  {
-     for(int i=0;i<v;i++)
-        line(p[i].x,p[i].y,p[i+1].x,p[i+1].y);
+
+  if (a[i].x<minx) minx=a[i].x;
+
+  if (a[i].x>maxx) maxx=a[i].x;
+
  }
- void nhapdagiac(ToaDo p[],int v)
+
+ //Quet tu minx --> maxx
+
+ for (x=minx+1;x<=maxx-1;x++)
+
  {
-     int i;
-     for(i=0;i<v;i++)
+
+  int m=0,z[50]; //so giao diem
+
+  //Duyet qua cac canh
+
+  for (int i=1;i<=n;i++)
+
+  {
+
+    int t=i+1; if (i==n) t=1;
+
+    int s=i-1; if (i==1) s=n;
+
+    if (x==a[i].x)
+
+    {
+
+     if((x>min(a[s].x,a[t].x))&&(x<max(a[s].x,a[t].x)))
+
      {
-         cout<<"\nnhap toa do dinh"<<i+1;
-         cout<<"\n\tx["<<(i+1)<<"] = ";cin>>p[i].x;
-         cout<<"\n\ty["<<(i+1)<<"] = ";cin>>p[i].y;
+
+       m++; z[m]=a[i].y;
 
      }
-     p[i].x=p[0].x;
-     p[i].y=p[0].y;
- }
-void scanline(ToaDo p[],int v)
-{
-    int xmin,xmax,ymin,ymax,c,mang[50];
-    xmin=xmax=p[0].x;
-    ymin=ymax=p[0].y;
-    for(int i=0;i<v;i++)
-    {
-        if(xmin>p[i].x) xmin=p[i].x;
-        if(xmax<p[i].x) xmax=p[i].x;
-        if(xmax>p[i].y) xmax=p[i].y;
-        if(xmax<p[i].y) xmax=p[i].y;
-        float y;
-        y=ymin+0.01;
-        while(y<=ymax)
-        {
-            int x,x1,x2,y1,y2,tg;
-            c=0;
-            for(int i=0;i<v;i++)
-            {
-                x1=p[i].x;
-                y1=p[i].y;
-                x2=p[i+1].x;
-                y2=p[i+1].y;
-                if(y2<y1){
-                    tg=x1;x1=x2;x2=tg;
-                    tg=y1;y1=y2;y2=tg;
-                }
-                if(y<=y2&&y>=y1){
-                    if(y1==y2) x=x1;
-                    else{
-                        x=((y-y1)*(x2-x1)/(y2-y1));
-                        x=x+x1;
-                    }
-                    if(x<=xmax && x>=xmin)
-                        mang[c++]=x;
-                }
-            }
-            for(int i=0;i<c;i+=2){
-                    delay(10);
-            line(mang[i],y,mang[i+1],y);
 
-            }
-            y++;
-        }
+     else
+
+       {
+
+             m++; z[m]=a[i].y;
+
+             m++; z[m]=a[i].y;
+
+       }
+
+    }
+
+    else
+
+     if((x>min(a[i].x,a[t].x))&&(x<max(a[t].x,a[i].x)))
+
+     {
+
+      ++m;
+
+      float r;
+
+   r = (a[t].y-a[i].y)/(a[t].x-a[i].x);
+
+      z[m]=(int)(r*(x-a[i].x))+a[i].y;
+
+     }
+
+  }
+
+  //Sap xep cac giao diem theo thu tu tang dan
+
+  for (int i=1;i<m;i++)
+
+   for (int k=i+1;k<=m;k++)
+
+    if (z[i]>z[k])
+
+    {
+
+     int tg=z[i];
+
+     z[i]=z[k];
+
+     z[k]=tg;
+
+    }
+
+  //To mau tu giao diem le dem giao diem chan
+
+  for (int k=1;k<=m-1;k++)
+
+   if (k%2!=0)
+   {
+        delay(10); line(x,z[k],x,z[k+1]);
+   }
+
+ }
+
 }
-}
+
 int main()
+
 {
-    initwindow(600,700);
-    int c1,v;
-    do{
-        cout<<"\n nhap vao so dinh cua da giac:";cin>>v;
-    }while(v<3);
-    ToaDo p[v];
-    nhapdagiac(p,v);
-    //cout<<"\nchon mau (0-15)";cin>>c1;
-    vedagiac(p,v);
-    setcolor(10);
-    scanline(p, v);
-    getch();
-    //closegraph();
+
+  int n,gd=0,gm;
+  ToaDo2D a[100];
+  Nhap(n,a);
+  initgraph(&gd,&gm,"d:\\tc\\bgi");
+  VeDaGiac(n,a);
+  Scanline(n,a);
+  getch();
+  closegraph();
+
 }
